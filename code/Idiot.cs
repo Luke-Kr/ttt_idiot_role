0eb0b9bd791dc0d96e9c30cc5615a873b47ae6a7
Luke-Kr <lukraakman@gmail.com>
2023-08-25 13:57:16 +0200

    Fixed karma
    Idiot killing traitors pre reveal doesn't lower their karma anymore

--- code/Idiot.cs
+++ code/Idiot.cs
@@ -91,10 +91,6 @@ namespace TerrorTown
 			}
 		}
 
-		public override IList<string> AdversaryTeams => (from i in Teams.RegisteredTeams
-                                                         where i.GetType() != typeof(Spectator)
-                                                         where i.TeamAlignment != TeamAlignment.NoEnemies && i.TeamAlignment != TeamAlignment.Traitor && i != this
-                                                         select i.TeamName).ToList();
         public override string VictimKillMessage => "You were killed by {0}. They were the Idiot";
         public override string RoleDescription => "You are an innocent Terrorist! But there are traitors around... Who can you trust, and who is out to fill you with bullets?\r\n\r\nWatch your back and work with your comrades to get out of this alive!";
         public override string IdentifyString => "{0} found the body of {1}. They were the Idiot";
@@ -115,6 +111,27 @@ namespace TerrorTown
                     MyGame.Current.OnTeamWin(this);
                 }
             }
+        }
+
+        public override bool ShouldWin()
+        {
+            var adversaryTeams = (from i in Teams.RegisteredTeams
+                               where i.GetType() != typeof(Spectator)
+                               where i.TeamAlignment != TeamAlignment.NoEnemies && i.TeamAlignment != TeamAlignment.Traitor && i != this
+                               select i.TeamName).ToList();
+            foreach (string adversaryTeam in adversaryTeams)
+            {
+                _ = TeamName;
+                _ = from i in Teams.GetByName(adversaryTeam).Players
+                    where i.LifeState == LifeState.Alive && i.IsValid
+                    select i.Name;
+                if (Teams.GetByName(adversaryTeam).Players.Any((Player i) => i.LifeState == LifeState.Alive && i.IsValid))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
